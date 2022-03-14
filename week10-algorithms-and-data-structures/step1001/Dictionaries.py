48be2c93b8ba8d66fd2e2a02da1b84081501d4f8
Nearchos Paspallis <NPaspallis@uclan.ac.uk>
2022-03-14 12:54:19 +0200

    Initial work on week 10

---
+++ week10-algorithms-and-data-structures/step1001/Dictionaries.py
@@ -0,0 +1,7 @@
+my_cities = ["Nicosia", "Athens", "London"]
+print("my cities: ", my_cities)
+my_cities.sort()
+print("my sorted cities: ", my_cities)
+nicosia_distances = {"Athens": 913, "London":3216}
+print(nicosia_distances)
+print(nicosia_distances["Athens"])
